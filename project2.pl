2b2c3e30b21c85f17b986e8b92f39057b6a6dafc
Sarah Parratt <skp220007@utdallas.edu>
2026-05-08 17:48:49 -0500

    Added shift and active workstation helpers

--- project2.pl
+++ project2.pl
@@ -10,3 +10,31 @@ plan(plan(Morning, Evening, Night)) :-
     schedule_shift(morning, Employees, Morning, EmployeesAfterMorning),
     schedule_shift(evening, EmployeesAfterMorning, Evening, EmployeesAfterEvening),
     schedule_shift(night, EmployeesAfterEvening, Night, []).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% schedule_shift(+Shift, +AvailableEmployees, -Schedule, -RemainingEmployees)
+%
+% Builds the schedule for one shift.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+schedule_shift(Shift, AvailableEmployees, Schedule, RemainingEmployees) :-
+    active_workstations(Shift, Workstations),
+    schedule_workstations(Shift, Workstations, AvailableEmployees, Schedule, RemainingEmployees).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% active_workstations(+Shift, -Workstations)
+%
+% Finds all workstations that are not idle during the given shift.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+active_workstations(Shift, Workstations) :-
+    findall(station(Station, Min, Max),
+            (workstation(Station, Min, Max), \+ is_idle(Station, Shift)),
+            Workstations).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% is_idle(+Station, +Shift)
+%
+% Checks if a workstation is idle during a shift.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+is_idle(Station, Shift) :-
+    current_predicate(workstation_idle/2),
+    workstation_idle(Station, Shift).
